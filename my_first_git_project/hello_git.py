a837da7c44484d01a73d6d39807c8b7648d12d43
Ruud Wassink <ruud.wassink@qirion.nl>
2022-05-18 21:06:44 +0200

    init

---
+++ my_first_git_project/hello_git.py
@@ -0,0 +1,19 @@
+
+
+def give_me_a_hello():
+    print("Git says hi")
+
+
+def give_me_a_goodbye():
+    pass
+
+
+
+
+if __name__ == "__main__":
+    print(
+        "the program starts here"
+    )
+    give_me_a_hello()
+
+    give_me_a_goodbye()
